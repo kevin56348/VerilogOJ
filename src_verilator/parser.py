a0a3b9e624ecb93e296cac9c22a4f3b3c5e9c654
silencekeep <1558498329@qq.com>
2025-01-23 05:40:41 +0800

    add parser

--- src_verilator/parser.py
+++ src_verilator/parser.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 from __future__ import print_function
+# from pprint import pprint
 import sys
 import os
 from optparse import OptionParser
@@ -18,11 +19,40 @@ def traverse_ast(ast, typex, x):
         x.append(ast)
     return x
 
+def convert_ports_to_ansi(module_def):
+    ports = []
+    items_to_remove = []
+    portlist = module_def.portlist.ports
+    port_names = set()
+    for port in portlist:
+        first = port.first if port.first is not None else pyverilog.vparser.ast.Inout(
+            pyverilog.vparser.ast.Variable('<<unk_name?>>'))
+        second = port.second if port.second is not None else pyverilog.vparser.ast.Wire(
+            pyverilog.vparser.ast.Variable('<<unk_name?>>'))
+        port = pyverilog.vparser.ast.Ioport(first=first, second=second, lineno=port.lineno)
+        if port.first is not None:
+            port_names.add(port.first.name)
+
+        ports.append(port)
+        # if isinstance(ioitem, vast.Ioport):
+    new_portlist = vast.Portlist(tuple(ports))
+    module_def.portlist = new_portlist
+
+    # pprint(port_names)
+
+    for item in module_def.items:
+        if isinstance(item, (vast.Wire, vast.Reg)):
+            for signal in item.list:
+                if signal.name in port_names:
+                    items_to_remove.append(signal)
+
+    module_def.items = [item for item in module_def.items
+                        if not any(signal.name in port_names for signal in getattr(item, 'list', []))]
+
 
 def main():
     optparser = OptionParser()
     (options, args) = optparser.parse_args()
-
     filelist = args
     for f in filelist:
         if not os.path.exists(f):
@@ -30,21 +60,15 @@ def main():
 
     ast, directives = parse(filelist)
 
-    ast.show()
+    # Traverse the AST to find all ModuleDefs
+    modules = traverse_ast(ast, "ModuleDef", [])
 
-    # generate code
-    # # no params, we assume
-    # params = vast.Paramlist(())
-    #
-    # ports = vast.Portlist(tuple(ioports))
-    #
-    # moduledef = traverse_ast(ast, "ModuleDef", [])[0]
-    #
-    # ast = vast.ModuleDef(moduledef.name, params, ports, tuple(decls))
-    #
-    # codegen = ASTCodeGenerator()
-    # rslt = codegen.visit(ast)
-    # print(rslt)
+    for module in modules:
+        convert_ports_to_ansi(module)
+
+    codegen = ASTCodeGenerator()
+    rslt = codegen.visit(ast)
+    print(rslt)
 
 
 if __name__ == '__main__':
